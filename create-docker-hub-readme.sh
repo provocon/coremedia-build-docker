f8f9a42ec0c13c8795b91743504fc80110951b51
Martin Goellnitz <m.goellnitz@provocon.de>
2026-02-21 13:00:29 +0100

    Typo in Script

--- create-docker-hub-readme.sh
+++ create-docker-hub-readme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright 2025 Martin Goellnitz
+# Copyright 2025-2026 Martin Goellnitz
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,7 +15,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 #
-# Create the flavour of the READM.md here to be used for docker hub.
+# Create the flavour of the README.md here to be used for docker hub.
 #
 cp README.md README-hub.md
 for src in $(grep ']: htt' README.md|sed -e 's/]:.http/_http/g') ; do
